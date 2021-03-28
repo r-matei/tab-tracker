ebabe37e5e0543b46bc80d7b02b9f49e9b0d9a12
r-matei <roxana.matei1209@gmail.com>
2021-03-28 19:54:23 +0300

    setting app the registration

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,19 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
 app.use(morgan('combined'))
-app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: true
+}))
+
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hello world'
+    message: `Hello ${req.body.email}!`
   })
 })
 
